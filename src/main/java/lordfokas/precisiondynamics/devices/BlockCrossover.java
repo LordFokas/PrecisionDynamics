becd50503ae90e0f490ae597e20591cd64a24837
LordFokas <lordfokas@gmail.com>
2018-04-12 19:48:38 +0100

    Itemstack localization

--- src/main/java/lordfokas/precisiondynamics/devices/BlockCrossover.java
+++ src/main/java/lordfokas/precisiondynamics/devices/BlockCrossover.java
@@ -2,9 +2,13 @@ package lordfokas.precisiondynamics.devices;
 
 import lordfokas.precisiondynamics.devices.base.BlockBase;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class BlockCrossover extends BlockBase {
     public BlockCrossover() {
@@ -15,5 +19,15 @@ public class BlockCrossover extends BlockBase {
     @Override
     public TileEntityCrossover createTileEntity(World world, IBlockState state) {
         return null;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> list, ITooltipFlag flag) {
+        list.add(I18n.format("tile.crossover.tooltip"));
+        list.add("");
+        list.add("\u00A7n"+I18n.format("tooltip.throughput3"));
+        list.add(I18n.format("tooltip.throughput.energy", "\u00A7e320 kRF/t"));
+        list.add(I18n.format("tooltip.throughput.fluid", "\u00A7e32 B/t"));
+        list.add(I18n.format("tooltip.throughput.item", "\u00A7e16 IS/t"));
     }
 }
--- src/main/java/lordfokas/precisiondynamics/devices/ItemBlockDevice.java
+++ src/main/java/lordfokas/precisiondynamics/devices/ItemBlockDevice.java
@@ -1,10 +1,18 @@
 package lordfokas.precisiondynamics.devices;
 
+import lordfokas.precisiondynamics.PrecisionDynamics;
 import lordfokas.precisiondynamics.devices.base.EnumVariant;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemBlockDevice extends ItemBlock {
     private final BlockDevice block;
@@ -14,6 +22,7 @@ public class ItemBlockDevice extends ItemBlock {
         this.block = block;
         setRegistryName(block.getRegistryName());
         setHasSubtypes(true);
+        setCreativeTab(PrecisionDynamics.instance.tab);
     }
 
     @Override
@@ -26,5 +35,19 @@ public class ItemBlockDevice extends ItemBlock {
     @Override
     public String getUnlocalizedName(ItemStack stack) {
         return block.getUnlocalizedName() + "_" + EnumVariant.values()[stack.getMetadata()].suffix;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<String> list, ITooltipFlag flag) {
+        list.add(I18n.format(getUnlocalizedName(stack) + ".tooltip"));
+        list.add("");
+        String rate = "\u00A7c*ERROR*";
+        switch(EnumVariant.values()[stack.getMetadata()]){
+            case ENERGY: rate = "\u00A7e320 kRF/t"; break;
+            case FLUID: rate = "\u00A7e32 B/t"; break;
+            case ITEM: rate = "\u00A7e16 IS/t"; break;
+        }
+        String translated = I18n.format("tooltip.throughput1", rate);
+        list.add(translated);
     }
 }
